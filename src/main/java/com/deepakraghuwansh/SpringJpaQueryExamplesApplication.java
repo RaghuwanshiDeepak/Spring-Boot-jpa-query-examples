4a0477e2dda1500c34cd96f30dcb37a7f4280f8f
abhishek <abhishek.purohit@oodles.io>
2021-09-23 11:32:25 +0530

    added a base

---
+++ src/main/java/com/deepakraghuwansh/SpringJpaQueryExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.deepakraghuwansh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaQueryExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaQueryExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deepakraghuwansh/SpringJpaQueryExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepakraghuwansh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaQueryExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
